832ab4fe71dfb4697e0e20627173db70be968383
HHH <2454334269@qq.com>
2024-10-30 21:14:54 +0800

    自写模型完成三分之一

--- workspace/transformerModule.py
+++ workspace/transformerModule.py
@@ -5,30 +5,47 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.normalization import LayerNorm
 
+# 股票内信息融合模块
 class Attention(nn.Module):
-    def __init__(self, d_feat, d_model, nhead, nhid, nlayers, dropout=0.5):
+    def __init__(self, d_model, dropout=0.2):
         super().__init__()
-        self.feature_layer = nn.Linear(d_feat, d_model)
-        self.pos_encoder = PositionalEncoding(d_model)
+        # self.feature_layer = nn.Linear(d_feat, d_model)
+        # self.pos_encoder = PositionalEncoding(d_model)
+        self.d_model = d_model
         self.Wq = nn.Linear(d_model, d_model, bias=False)
         self.Wk = nn.Linear(d_model, d_model, bias=False)
         self.Wv = nn.Linear(d_model, d_model, bias=False)
+        self.dropout = nn.Dropout(dropout)
+        self.norm1 = LayerNorm(d_model)
 
-        # self.norm1 = LayerNorm(d_model)
 
-
-    def forward(self, src):
-        x = self.pos_encoder(src) # [N, T, F]
-        x = self.pos_encoder(x)  
+    def forward(self, x):
+        # x = self.pos_encoder(x)  
         q = self.Wq(x)
         k = self.Wk(x)
         v = self.Wv(x)
-        _,_,_,d = k.size()
-        kt = torch.transpose(k, 2, 3)
-        s = (q @ kt) / math.sqrt(d)
+        kt = torch.transpose(k, 1, 2)
+        s = (q @ kt) / math.sqrt(self.d_model)
         v = s @ v
         return v
+    
+# 前馈网络模块
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, d_model, d_ff=512, dropout=0.2):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(d_ff, 1)
+        self.dropout = nn.Dropout(dropout)
 
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+
+# 位置编码模块
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=1000):
         super(PositionalEncoding, self).__init__()
@@ -39,23 +56,24 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
         # self.register_buffer 是一个用于向模型（通常是nn.Module的子类）注册一个不需要梯度的张量（tensor）的方法。
         # 注册的张量将会保存在state_dict中，并在模型保存和加载时被持久化。
         self.register_buffer("pe", pe)
 
     def forward(self, x):
-        # [T, N, F]：N股票数、T历史窗口、F特征数
-        return x + self.pe[:x.size(0), :]
+        # [T, N, F]：天数、T历史窗口、F特征数
+        return x + self.pe[: x.size(0), :]
 
-# class AddNorm(nn.modules):
-#     def __init__(self, d_model, dropout=0.1):
-#         super(AddNorm, self).__init__()
-#         self.norm = LayerNorm(d_model)
-#         self.dropout = nn.Dropout(dropout)
+class AddNormLayer(nn.Module):
+    def __init__(self, d_model, dropout=0.1):
+        super(AddNormLayer, self).__init__()
+        self.norm = LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
 
-#     def forward(self, input, x):
-#         input = self.dropout(input)
-#         return self.norm(input + x)
+    def forward(self, input, src):
+        input = self.dropout(input)
+        return self.norm(input+ src) 
 
 class FeedForward(nn.Module):
     def __init__(self, d_model):
