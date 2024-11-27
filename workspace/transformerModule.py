5e2e90cd993cefe92bac1a9def545dedd197156f
HHH <2454334269@qq.com>
2024-11-27 21:56:02 +0800

    种子及网络模型参数打印

--- workspace/transformerModule.py
+++ workspace/transformerModule.py
@@ -5,6 +5,61 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.normalization import LayerNorm
 
+class Attention(nn.Module):
+    """
+    Compute 'Scaled Dot Product Attention
+    """
+
+    def forward(self, query, key, value, mask=None, dropout=None):
+        scores = torch.matmul(query, key.transpose(-2, -1)) \
+                 / math.sqrt(query.size(-1))
+
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+
+        p_attn = F.softmax(scores, dim=-1)
+
+        if dropout is not None:
+            p_attn = dropout(p_attn)
+
+        return torch.matmul(p_attn, value), p_attn
+
+# bert的多头注意力模块
+class MultiHeadedAttention(nn.Module):
+    """
+    Take in model size and number of heads.
+    """
+
+    def __init__(self, h, d_model, dropout=0.1):
+        super().__init__()
+        assert d_model % h == 0
+
+        # We assume d_v always equals d_k
+        self.d_k = d_model // h
+        self.h = h
+
+        self.linear_layers = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(3)])
+        self.output_linear = nn.Linear(d_model, d_model)
+        self.attention = Attention()
+
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size(0)
+
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linear_layers, (query, key, value))]
+
+        # 2) Apply attention on all the projected vectors in batch.
+        x, attn = self.attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # 3) "Concat" using a view and apply a final linear.
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_k)
+
+        return self.output_linear(x)
+
+
 # 多头注意力模块
 class MultiHeadAttention(nn.Module):
     def __init__(self, n_heads=8, d_model=512, dropout=0.2):
@@ -15,8 +70,9 @@ class MultiHeadAttention(nn.Module):
         self.Wk = nn.Linear(d_model, d_model)
         self.Wv = nn.Linear(d_model, d_model)
         self.dropout = nn.Dropout(p=dropout)
-        self.norm = LayerNorm(d_model)
+        # self.norm = LayerNorm(d_model)
         self.softmax = nn.Softmax(dim=-1)
+        self.o_linear = nn.Linear(d_model, d_model)
 
     def forward(self, hidden_state, attention_mask=None):
         # hidden_state 形状: (batch_size, seq_len, hidden_size)
@@ -56,7 +112,7 @@ class MultiHeadAttention(nn.Module):
         output = output.transpose(1, 2).reshape(batch_size, -1, dim * self.n_heads)
         
         # 通过线性层将拼接后的输出变换为所需的输出维度
-        # output = self.o_linear(output)  # (batch_size, seq_len, hidden_size)
+        output = self.o_linear(output)  # (batch_size, seq_len, hidden_size)
         
         return output
     def split_head(self, x, dim):
@@ -83,6 +139,8 @@ class FeedForwardNetwork(nn.Module):
         x = self.dropout(x)
         x = self.linear2(x)
         return x
+
+
 
 # 位置编码模块
 class PositionalEncoding(nn.Module):
